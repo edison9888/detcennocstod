2550a0738a1a92e121d5182c1778f6d2ba5f49d3
seeskyline <wen.shane@gmail.com>
2012-11-26 10:11:06 +0800

    1st commit

---
+++ DotsConnected/DotsConnected/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DotsConnected
+//
+//  Created by Wen Shane on 12-11-26.
+//  Copyright (c) 2012年 Wen Shane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
