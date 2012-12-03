365f2fda12f7ede6bb461fef5ad7bc66c9b7b32d
seeskyline <wen.shane@gmail.com>
2012-12-03 22:31:16 +0800

    refine the code and add simple icons

---
+++ project/DotsConnected/CanvasView.h
@@ -0,0 +1,13 @@
+//
+//  CanvasView.h
+//  DotsConnected
+//
+//  Created by Wen Shane on 12-12-3.
+//  Copyright (c) 2012年 Wen Shane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CanvasView : UIView
+
+@end
---
+++ project/DotsConnected/CanvasView.m
@@ -0,0 +1,31 @@
+//
+//  CanvasView.m
+//  DotsConnected
+//
+//  Created by Wen Shane on 12-12-3.
+//  Copyright (c) 2012年 Wen Shane. All rights reserved.
+//
+
+#import "CanvasView.h"
+
+@implementation CanvasView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
